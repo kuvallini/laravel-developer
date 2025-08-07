decd9d63fed8953327750c3d376da63f2a52f248
kuvallini <alex@kuvallini.com>
2025-08-07 19:30:07 +0200

    Task 4

--- app/Http/Controllers/GolferController.php
+++ app/Http/Controllers/GolferController.php
@@ -44,4 +44,46 @@ class GolferController extends Controller
 
         return response()->json($golfers);
     }
+
+    /**
+     * Added: Alex Kuvallini
+     * Exporting results in CSV for API calls: /api/nearest-golfers/csv?lat=52.5200&lng=13.4050
+     */
+    public function nearestCsv(Request $request)
+    {
+        $golfers = $this->nearestQuery($request);
+
+        $filename = 'nearest_golfers.csv';
+        $headers = [
+            "Content-type"        => "text/csv",
+            "Content-Disposition" => "attachment; filename=$filename",
+            "Pragma"              => "no-cache",
+            "Cache-Control"       => "must-revalidate, post-check=0, pre-check=0",
+            "Expires"             => "0"
+        ];
+
+        $columns = ['ID', 'Debitor Account', 'Name', 'Email', 'Date of birth', 'Latitude', 'Longitude', 'Distance (km)'];
+
+        $callback = function() use ($golfers, $columns) {
+            $file = fopen('php://output', 'w');
+            fputcsv($file, $columns);
+
+            foreach ($golfers as $golfer) {
+                fputcsv($file, [
+                    $golfer->id,
+                    $golfer->debitor_account,
+                    $golfer->name,
+                    $golfer->email,
+                    $golfer->born_at,
+                    $golfer->latitude,
+                    $golfer->longitude,
+                    round($golfer->distance, 2),
+                ]);
+            }
+
+            fclose($file);
+        };
+
+        return response()->stream($callback, 200, $headers);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('/nearest-golfers', [GolferController::class, 'nearestGet']);
+Route::get('/nearest-golfers/csv', [GolferController::class, 'nearestCsv']);
