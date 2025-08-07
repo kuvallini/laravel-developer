91756fd8d7e42e9285d04b6afc70e0c9c4084b61
kuvallini <alex@kuvallini.com>
2025-08-07 19:00:47 +0200

    Task 1 & 2

--- app/Models/Golfer.php
+++ app/Models/Golfer.php
@@ -27,4 +27,15 @@ class Golfer extends Model
         'latitude' => 'float',
         'longitude' => 'float',
     ];
+
+    /**
+     * Added: Alex Kuvallini
+     * Autoincrease the debitor account number based on the lastest number in the golfer database
+     */
+    public static function booted(): void
+    {
+        static::creating(function (self $table) {
+            $table->debitor_account = $table->max('debitor_account') + 1;
+        });
+    }
 }
